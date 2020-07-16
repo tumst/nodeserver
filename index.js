b3627437f22d1fe29f9c9575fffe8c482919a990
Sa-ard <tumoioio@gmail.com>
2020-07-16 08:21:34 +0700

    create index.js

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const express = require('express');
 
 const app = express();
 
+// define env
+const PORT = process.env.PORT || 5000
+
+
+
+
 app.get("/", function(req, res){
     res.send("Hello world.");
 });
 
+app.listen(PORT, () => console.log(`Listening on ${ PORT }.`) );
